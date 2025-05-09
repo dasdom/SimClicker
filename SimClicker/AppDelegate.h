fca9e43e896fb580e99d52e5cf49fa7abd94d9bc
Dominik Hauser <dom@dasdom.de>
2025-05-09 17:33:42 +0200

    Initial Commit

---
+++ SimClicker/AppDelegate.h
@@ -0,0 +1,12 @@
+//  Created by Dominik Hauser on 09.05.25.
+//  
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SimClicker/main.m
@@ -0,0 +1,13 @@
+//  Created by Dominik Hauser on 09.05.25.
+//  
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+  }
+  return NSApplicationMain(argc, argv);
+}
